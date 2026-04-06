2b4c54beef2e68d6ad8f09c51003d1de071e3f19
Vipul Modi <vmodi00@gmail.com>
2026-04-06 22:48:15 +0530

    Assignment 1: Add to cart

--- tests/Assignment_1.spec.js
+++ tests/Assignment_1.spec.js
@@ -1,11 +1,27 @@
 const { test, expect } = require('@playwright/test');
 
 test('Register on Practice automation', async ({ page }) => {
+
+    const productName = 'ZARA COAT 3';
+    const products = page.locator('.card-body');
     await page.goto("https://rahulshettyacademy.com/client");
     await page.locator('#userEmail').fill("vipulmodiqa@gmail.com");
     await page.locator('#userPassword').fill("Test@1234");
     await page.locator('#login').click();
-    //await page.waitForLoadState('networkidle');
+    // await page.waitForLoadState('networkidle');
     await page.locator('.card-body b').first().waitFor();
-    console.log(await page.locator('.card-body b').allTextContents());
+    const titles = await page.locator('.card-body b').allTextContents();
+    console.log(titles);
+    const count = await products.count();
+    for (let i = 0; i < count; ++i) {
+        if (await products.nth(i).locator("b").textContent() === productName) {
+            await products.nth(i).locator("text = Add To Cart").click();
+            break;
+        }
+    }
+    await page.locator("button[routerlink*='cart']").click();
+    await page.locator("div li").first().waitFor();
+    const bool = await page.locator(`h3:has-text('${productName}')`).isVisible();
+    expect(bool).toBeTruthy();
+    await page.pause();
 });
