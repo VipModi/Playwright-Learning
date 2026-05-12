b51d494edc28401318389ff140a3d9fe9e598c51
Vipul Modi <vmodi00@gmail.com>
2026-05-12 23:05:27 +0530

    API Testing : Create Order

--- tests/Lesson_5.spec.js
+++ tests/Lesson_5.spec.js
@@ -2,7 +2,9 @@ const { test, expect, request } = require("@playwright/test");
 // const { url } = require("node:inspector");
 // const { json } = require("node:stream/consumers");
 const loginPayLoad = { userEmail: "vipulmodiqa@gmail.com", userPassword: "Test@1234" };
+const orderPayLoad = { orders: [{ country: "India", productOrderedId: "6960ea76c941646b7a8b3dd5" }] }
 let token;
+let orderId;
 
 test.beforeAll(async () => {
     const apiContext = await request.newContext();
@@ -16,6 +18,18 @@ test.beforeAll(async () => {
     const loginResponseJson = await loginResponse.json();
     token = loginResponseJson.token;
     console.log(token);
+
+    const orderResponse = await apiContext.post("https://rahulshettyacademy.com/api/ecom/order/create-order",
+        {
+            data: orderPayLoad,
+            headers: {
+                'Authorization': token,
+                'Content-Type': 'application/json'
+            },
+        })
+
+    const orderResponseJson = await orderResponse.json();
+    orderId = orderResponseJson.orders[0];
 });
 
 test.beforeEach(() => {
@@ -24,61 +38,21 @@ test.beforeEach(() => {
 
 test('Testing API testing', async ({ page }) => {
 
-    await page.addInitScript(value => {window.localStorage.setItem('token', value);}, token);
-    const productName = 'ZARA COAT 3';
-    await page.goto("https://rahulshettyacademy.com/client/#/dashboard/dash");
-    const products = page.locator('.card-body');
-    await page.waitForLoadState('networkidle');
-    await page.locator('.card-body b').first().waitFor();
-    const titles = await page.locator('.card-body b').allTextContents();
-    console.log(titles);
-    const count = await products.count();
-    for (let i = 0; i < count; ++i) {
-        if (await products.nth(i).locator("b").textContent() === productName) {
-            await products.nth(i).locator("text = Add To Cart").click();
-            break;
-        }
-    }
-    await page.locator("button[routerlink*='cart']").click();
-    await page.locator("div li").first().waitFor();
-    const bool = await page.locator(`h3:has-text('${productName}')`).isVisible();
-    expect(bool).toBeTruthy();
-    await page.locator("text=Checkout").click();
-    await page.locator("input[placeholder='Select Country']").pressSequentially("ind", { delay: 100 });
-    const dropdown = page.locator(".ta-results")
-    await dropdown.waitFor();
-    const countryCount = await dropdown.locator(".ta-item").count();
-    for (let i = 0; i < countryCount; i++) {
-        const text = await dropdown.locator(".ta-item").nth(i).textContent();
-        if (text.trim() === "India") {
-            await dropdown.locator(".ta-item").nth(i).click();
-            break;
-        }
-    }
-    await page.locator('.field [type="text"]').nth(1).fill(123 + "");
-    await page.locator('.field [type="text"]').nth(2).fill("Vipul Modi");
-    await page.locator("input[name='coupon']").fill("rahulshettyacademy");
-    await page.locator("button[type='submit']").click();
-    await expect(page.locator('text="* Coupon Applied"')).toBeVisible();
-    console.log(await page.locator('text="* Coupon Applied"').textContent());
-    await page.locator(".actions .btnn").click();
-    await expect(page.locator("text=Thankyou for the order.")).toBeVisible();
-    console.log(await page.locator("text=Thankyou for the order.").textContent());
-    let testOrder = await page.locator("label[class='ng-star-inserted']").textContent();
-    testOrder = testOrder.split(" ")[2];
+    await page.addInitScript(value => { window.localStorage.setItem('token', value); }, token);
+    await page.goto("https://rahulshettyacademy.com/client/");
     await page.locator("button[routerlink*='myorders']").click();
     await page.locator('.table [scope="row"]').first().waitFor();
     const orderIds = await page.locator('.table [scope="row"]').allTextContents();
     for (let i = 0; i < orderIds.length; ++i) {
-        //if(orderIds[i].includes(testOrder)){
-        if (orderIds[i] === testOrder) {
-            await expect(page.locator(".table [scope='row']").nth(i)).toHaveText(testOrder);
+        //if(orderIds[i].includes(orderId)){
+        if (orderIds[i] === orderId) {
+            await expect(page.locator(".table [scope='row']").nth(i)).toHaveText(orderId);
             await page.locator("[class='btn btn-primary']").nth(i).click();
             break;
         }
     }
     const OrderIDDetais = await page.locator(".col-text").textContent();
-    await expect(OrderIDDetais.includes(testOrder)).toBeTruthy();
+    await expect(OrderIDDetais.includes(orderId)).toBeTruthy();
 
 
 });
--- tests/Temp.spec.js
+++ tests/Temp.spec.js
@@ -1,15 +1,79 @@
-const { test, expect } = require('@playwright/test');
-const { assert } = require('node:console');
+// const { test, expect } = require('@playwright/test');
+// const { assert } = require('node:console');
 
-test.only('Instagram Login Test', async ({ page }) => {
-    const bday = '10/27/1999';
-    const monthNumber = "10";
-    const dateNumber = "27";
-    const yearNumber = "1999";
-    await page.goto("https://demoqa.com/date-picker");
-    await page.locator('//input[@id="datePickerMonthYearInput"]').click();
-    await page.locator(".react-datepicker__year-select").selectOption(yearNumber);
-    await page.locator('.react-datepicker__month-select').selectOption((monthNumber - 1).toString());
-    await page.locator('[class="react-datepicker__month"]').getByText(dateNumber).last().click();
-    await expect(page.locator('#datePickerMonthYearInput')).toHaveValue(bday);
+// test.only('Instagram Login Test', async ({ page }) => {
+//     const bday = '10/27/1999';
+//     const monthNumber = "10";
+//     const dateNumber = "27";
+//     const yearNumber = "1999";
+//     await page.goto("https://demoqa.com/date-picker");
+//     await page.locator('//input[@id="datePickerMonthYearInput"]').click();
+//     await page.locator(".react-datepicker__year-select").selectOption(yearNumber);
+//     await page.locator('.react-datepicker__month-select').selectOption((monthNumber - 1).toString());
+//     await page.locator('[class="react-datepicker__month"]').getByText(dateNumber).last().click();
+//     await expect(page.locator('#datePickerMonthYearInput')).toHaveValue(bday);
+// });
+
+const { test, expect, request } = require('@playwright/test');
+const PayLoadLogin = { userEmail: "bunnyshanon786@gmail.com", userPassword: "Sneh@2511" }
+const OrderPayload = {
+    orders: [
+        {
+            country: "India",
+            productOrderedId: "6960eae1c941646b7a8b3ed3"
+        }
+    ]
+}
+let token;
+let orderId;
+
+test.beforeAll(async () => {
+
+    const ApiContext = await request.newContext();
+    const LoginResponse = await ApiContext.post('https://rahulshettyacademy.com/api/ecom/auth/login', { data: PayLoadLogin });
+    expect(LoginResponse.ok()).toBeTruthy();
+    const LoginResponseJson = await LoginResponse.json();
+    token = LoginResponseJson.token;
+    console.log(token);
+
+    const OrderResponse = await ApiContext.post('https://rahulshettyacademy.com/api/ecom/order/create-order', {
+        data: OrderPayload,
+        headers: {
+            'Authorization': token,
+            'Content-Type': "application/json"
+        },
+
+    })
+    const OrderResponseJson = await OrderResponse.json();
+    orderId = OrderResponseJson.orders[0];
+    console.log(orderId);
+
+});
+
+test.beforeEach(() => {
+
+});
+
+test('Register on Practice automation', async ({ page }) => {
+
+    await page.addInitScript(value => {
+        window.localStorage.setItem('token', value);
+    }, token);
+
+    await page.goto("https://rahulshettyacademy.com/client/");
+    await page.locator("button[routerlink*='myorders']").click();
+    await page.locator('.table [scope="row"]').first().waitFor();
+    const orderIds = await page.locator('.table [scope="row"]').allTextContents();
+    for (let i = 0; i < orderIds.length; ++i) {
+        //if(orderIds[i].includes(orderId)){
+        if (orderIds[i] === orderId) {
+            await expect(page.locator(".table [scope='row']").nth(i)).toHaveText(orderId);
+            await page.locator("[class='btn btn-primary']").nth(i).click();
+            break;
+        }
+    }
+    const OrderIDDetais = await page.locator(".col-text").textContent();
+    await expect(OrderIDDetais.includes(orderId)).toBeTruthy();
+
+
 });
